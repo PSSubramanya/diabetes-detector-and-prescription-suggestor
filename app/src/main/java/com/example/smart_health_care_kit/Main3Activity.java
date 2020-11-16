3bc740a387230ee105efa2da064f25f98bb8d3e6
PS Subramanya Bhat <subramanya@robosoftin.com>
2020-11-17 02:46:33 +0530

    Added A Medical App which is used to type of Diabetes and suggest basic prescription based on that

--- app/src/main/java/com/example/smart_health_care_kit/Main3Activity.java
+++ app/src/main/java/com/example/smart_health_care_kit/Main3Activity.java
@@ -10,11 +10,36 @@ import android.widget.CheckBox;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class Main3Activity extends AppCompatActivity {
-    TextView dispview,prescp,cktxt1,cktxt2,cktxt3,cktxt4,cktxt5,diab;
-    CheckBox ck1,ck2,ck3,ck4,ck5;
+
+    int preDiabeticFlag = 0;
+    int diabeticType2Flag = 0;
+
+    final String preDiabeticMedicines = "Metaglip\n" +
+            "Glucovsnce\n"+
+            "Actoplus Met\n"+
+            "Prandimet\n"+
+            "Kombiglyze\n"+
+            "Janumet\n";
+
+    final String diabeticType2Medicines = "Bydureon (exenatide)\n"+
+            "Humalog\n" +
+            "Jardiance\n" +
+            "Lantus\n" +
+            "Soliqua 100/33\n" +
+            "Toujeo\n" +
+            "Trulicity\n";
+
+
+
+
+    TextView dispview,prescp,cktxt1,cktxt2,diab;
+//    cktxt3,cktxt4,cktxt5,;
+    CheckBox ck1,ck2;
+//    ck3,ck4,ck5;
     EditText sug;
     Button checking;
     @Override
@@ -22,33 +47,30 @@ public class Main3Activity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main3);
 
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
+
         dispview = (TextView)findViewById(R.id.dispview);
 //        text2 = (TextView)findViewById(R.id.text2);
         prescp = (TextView)findViewById(R.id.prescp);
         ck1 = (CheckBox)findViewById(R.id.ck1);
         ck2 = (CheckBox)findViewById(R.id.ck2);
-        ck3 = (CheckBox)findViewById(R.id.ck3);
-        ck4 = (CheckBox)findViewById(R.id.ck4);
-        ck5 = (CheckBox)findViewById(R.id.ck5);
+//        ck3 = (CheckBox)findViewById(R.id.ck3);
+//        ck4 = (CheckBox)findViewById(R.id.ck4);
+//        ck5 = (CheckBox)findViewById(R.id.ck5);
         cktxt1 = (TextView)findViewById(R.id.cktxt1);
         cktxt2 = (TextView)findViewById(R.id.cktxt2);
-        cktxt3 = (TextView)findViewById(R.id.cktxt3);
-        cktxt4 = (TextView)findViewById(R.id.cktxt4);
-        cktxt5 = (TextView)findViewById(R.id.cktxt5);
+//        cktxt3 = (TextView)findViewById(R.id.cktxt3);
+//        cktxt4 = (TextView)findViewById(R.id.cktxt4);
+//        cktxt5 = (TextView)findViewById(R.id.cktxt5);
         sug = (EditText)findViewById(R.id.sug);
         checking = (Button)findViewById(R.id.checking);
         diab = (TextView) findViewById(R.id.diab);
 
-        String name = getIntent().getStringExtra("result");
-        dispview.setText("\t\t\t\t\t\t"+name);
-        if (name.equals("Diabetes"))
-        {
-            ck3.setVisibility(View.INVISIBLE);
-            cktxt3.setVisibility(View.INVISIBLE);
-            ck4.setVisibility(View.INVISIBLE);
-            cktxt4.setVisibility(View.INVISIBLE);
-            ck5.setVisibility(View.INVISIBLE);
-            cktxt5.setVisibility(View.INVISIBLE);
+//        String name = getIntent().getStringExtra("result");
+        dispview.setText("Enter the readings");
+
+
             checking.setOnClickListener(new View.OnClickListener() {
                 @Override
                 public void onClick(View view) {
@@ -61,23 +83,44 @@ public class Main3Activity extends AppCompatActivity {
 
                         String sug1 = sug.getText().toString();
                         int valc = Integer.parseInt(sug1);
-                        if (valc >100 && valc<=125)
+
+                        if (valc >=100 && valc<=125)
                         {
-                            prescp.setText("You are affected by Diabetes\n" +
-                                    "Kazano\nOseni\nMetaglip\nGlucovance\nJentadueto\nDuetact\nActoplus\n");
+//                            prescp.setText("You are affected by Diabetes\n" +
+//                                    "Kazano\nOseni\nMetaglip\nGlucovance\nJentadueto\nDuetact\nActoplus\n");
+
+//                            prescp.setText("You are affected by Prediabetes\n\nMedicines prescribed: \n"+preDiabeticMedicines);
+                            if (valc>=100 && valc<=120)
+                            {
+                                prescp.setText("You are affected by Prediabetes\nInsulin dose intake: 2 (IU/d)\nMedicines prescribed:\n"+preDiabeticMedicines);
+                            }
+                            else if (valc>120 && valc<140)
+                            {
+                                prescp.setText("You are affected by Prediabetes\nInsulin dose intake: 4 (IU/d)\nMedicines prescribed:\n"+preDiabeticMedicines);
+                            }
+                            preDiabeticFlag++;
                         }
                         else if (valc<100)
                         {
-                            prescp.setText("You are affected by Prediabetes\nMetaglip\n" +
-                                    "Glucovsnce\n"+
-                                    "Actoplus Met\n"+
-                                    "Prandimet\n"+
-                                    "Kombiglyze\n"+
-                                    "Janumet\n");
+                            prescp.setText("Normal\n You are healthy");
                         }
                         else
                         {
-                            prescp.setText("Normal\n You are healthy");
+//                            prescp.setText("Affected by Diabetes type2\n\nMedicines prescribed: \n" + diabeticType2Medicines);
+                            if (valc>125 && valc<140)
+                            {
+                                prescp.setText("Affected by Diabetes type2\nInsulin dose intake: 4 (IU/d)\nMedicines prescribed: \n" + diabeticType2Medicines);
+                            }
+                            else if (valc>=140 && valc<180)
+                            {
+                                prescp.setText("Affected by Diabetes type2\nInsulin dose intake: 6 (IU/d)\nMedicines prescribed: \n" + diabeticType2Medicines);
+                            }
+                            else if (valc>=180)
+                            {
+                                prescp.setText("Affected by Diabetes type2\nInsulin dose intake: 8 (IU/d)\nMedicines prescribed: \n" + diabeticType2Medicines);
+                            }
+                            diabeticType2Flag++;
+
                         }
 
 
@@ -88,92 +131,92 @@ public class Main3Activity extends AppCompatActivity {
                         cktxt1.setVisibility(View.INVISIBLE);
                         String sug1 = sug.getText().toString();
                         int valc = Integer.parseInt(sug1);
-                        if (valc<180)
+                        if (valc<140)
                         {
-                            prescp.setText("Normal\n YOu are healthy");
+                            prescp.setText(" Normal\n You are healthy");
+                        }
+                        else if(valc>=140 && valc<=199)
+                        {
+//                            prescp.setText("Diabetic\n" +
+//                                    "Kazano\nOseni\nMetaglip\nGlucovance\nJentadueto\nDuetact\nActoplus\n");
+
+//                            prescp.setText("You are affected by Prediabetes\n\nMedicines prescribed: \n"+preDiabeticMedicines);
+
+                            //insulin in take value code
+                            if (valc<=150)
+                            {
+                                prescp.setText("You are affected by Prediabetes\nInsulin dose intake: 0 (IU/d)\nMedicines prescribed: \n"+preDiabeticMedicines);
+                            }
+                            else if (valc>150 && valc<=200)
+                            {
+                                prescp.setText("You are affected by Prediabetes\nInsulin dose intake: 3 (IU/d)\nMedicines prescribed: \n"+preDiabeticMedicines);
+                            }
+                            preDiabeticFlag++;
                         }
                         else
                         {
-                            prescp.setText("Diabetic\n" +
-                                    "Kazano\nOseni\nMetaglip\nGlucovance\nJentadueto\nDuetact\nActoplus\n");
+//                            prescp.setText("Affected by Diabetes type2\n\nMedicines prescribed: \n" + diabeticType2Medicines);
+                            if (valc>=200 && valc<=250)
+                            {
+                                prescp.setText("Affected by Diabetes type2\nInsulin dose intake: 5 (IU/d)\nMedicines prescribed: \n" + diabeticType2Medicines);
+                            }
+                            else if (valc>250 && valc<=300)
+                            {
+                                prescp.setText("Affected by Diabetes type2\nInsulin dose intake: 8 (IU/d)\nMedicines prescribed: \n" + diabeticType2Medicines);
+                            }
+                            else if (valc>300 && valc<=350)
+                            {
+                                prescp.setText("Affected by Diabetes type2\nInsulin dose intake: 10 (IU/d)\nMedicines prescribed: \n" + diabeticType2Medicines);
+                            }
+                            else if (valc>350 && valc<=400)
+                            {
+                                prescp.setText("Affected by Diabetes type2\nInsulin dose intake: 12 (IU/d)\nMedicines prescribed: \n" + diabeticType2Medicines);
+                            }
+                            if (valc>400)
+                            {
+                                prescp.setText("Affected by Diabetes type2\nInsulin dose intake: 15 (IU/d)\nMedicines prescribed: \n" + diabeticType2Medicines);
+                            }
+                            diabeticType2Flag++;
                         }
                     }
                     //prescp.setText("Hi");
 
                 }
             });
-        }
-
-        else if (name.equals("BP"))
-        {
-            ck1.setVisibility(View.INVISIBLE);
-            cktxt1.setVisibility(View.INVISIBLE);
-            ck2.setVisibility(View.INVISIBLE);
-            cktxt2.setVisibility(View.INVISIBLE);
-            diab.setVisibility(View.INVISIBLE);
-            sug.setVisibility(View.INVISIBLE);
-            //checking.setVisibility(View.INVISIBLE);
-            checking.setOnClickListener(new View.OnClickListener() {
-                @Override
-                public void onClick(View view) {
-                    boolean check3 = ck3.isChecked();
-                    boolean check4 = ck4.isChecked();
-                    boolean check5 = ck5.isChecked();
-                    if (check3)
-                    {
-                        ck4.setVisibility(View.INVISIBLE);
-                        cktxt4.setVisibility(View.INVISIBLE);
-                        ck5.setVisibility(View.INVISIBLE);
-                        cktxt5.setVisibility(View.INVISIBLE);
-
-                        prescp.setText("Low BP");
+//        }
 
 
-                    }
-                    else if (check4)
-                    {
-                        ck3.setVisibility(View.INVISIBLE);
-                        cktxt3.setVisibility(View.INVISIBLE);
-                        ck5.setVisibility(View.INVISIBLE);
-                        cktxt5.setVisibility(View.INVISIBLE);
-
-                        prescp.setText("Normal BP");
 
 
-                    }
-                    else if (check5)
-                    {
-                        ck3.setVisibility(View.INVISIBLE);
-                        cktxt3.setVisibility(View.INVISIBLE);
-                        ck4.setVisibility(View.INVISIBLE);
-                        cktxt4.setVisibility(View.INVISIBLE);
-
-                        prescp.setText("High BP");
 
 
-                    }
-                }
-            });
-        }
-//        ck1.setVisibility(View.VISIBLE);
-//        cktxt1.setVisibility(View.VISIBLE);
-//        ck2.setVisibility(View.VISIBLE);
-//        cktxt2.setVisibility(View.VISIBLE);
-//        diab.setVisibility(View.VISIBLE);
-//        sug.setVisibility(View.VISIBLE);
-//        checking.setVisibility(View.VISIBLE);
+
+
+
     }
-    //    public  void compare()
-//    {
-//        int vals;
-//        vals = sug.getText().toString();
-//        if ()
-//    }
     public void order(View view)
     {
-        Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:7204958072"));
-        intent.putExtra("sms_body","We are having shortage of the prescribed medical drug\n" +
-                "So we kindly request you to deliver the medicines to our address as soon as possible.");
+        Intent intent = new Intent(Intent.ACTION_SENDTO, Uri.parse("smsto:8891792177"));
+
+        if (preDiabeticFlag>0)
+        {
+            intent.putExtra("sms_body","We are having shortage of the prescribed medical drug\n" +
+                    "So we kindly request you to deliver the medicines to our address as soon as possible.\n"+
+                    "The prescribed medicines:\n"+
+                    preDiabeticMedicines);
+        }
+        if (diabeticType2Flag>0)
+        {
+            intent.putExtra("sms_body","We are having shortage of the prescribed medical drug\n" +
+                    "So we kindly request you to deliver the medicines to our address as soon as possible.\n"+
+                    "The prescribed medicines:\n"+
+                    diabeticType2Medicines);
+        }
+//        intent.putExtra("sms_body","We are having shortage of the prescribed medical drug\n" +
+//                "So we kindly request you to deliver the medicines to our address as soon as possible.\n"+
+//                "The prescribed medicines:\n");
+        preDiabeticFlag = 0;
+        diabeticType2Flag = 0;
         startActivity(intent);
     }
 }
--- app/src/main/java/com/example/smart_health_care_kit/MainActivity.java
+++ app/src/main/java/com/example/smart_health_care_kit/MainActivity.java
@@ -1,40 +1,80 @@
 package com.example.smart_health_care_kit;
 
+import android.Manifest;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
+import androidx.annotation.RequiresApi;
+import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends AppCompatActivity {
     TextView marq1;
-    Button chkp,mediinfo;
+    Button mediinfo, calldoc;
+    private static final int REQUEST_CALL = 1;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.hide();
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         marq1 = (TextView)findViewById(R.id.marq1);
         marq1.setSelected(true);
-        chkp=(Button)findViewById(R.id.chkp);
         mediinfo=(Button)findViewById(R.id.mediinfo);
+        calldoc=(Button)findViewById(R.id.calldoc);
 
-        chkp.setOnClickListener(new View.OnClickListener() {
+
+        calldoc.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,Main4Activity.class);
-                startActivity(intent);
+                DialToDoc();
             }
         });
+
+
 
         mediinfo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
+                Intent intent = new Intent(MainActivity.this,Main3Activity.class);
+//                Intent intent = new Intent(MainActivity.this,Main2Activity.class);
                 startActivity(intent);
             }
         });
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    public void DialToDoc() {
+//        Intent intent = new Intent(Intent.ACTION_CALL);
+//        intent.setData(Uri.parse("tel:8891792177"));
+        if (checkSelfPermission(Manifest.permission.CALL_PHONE) != PackageManager.PERMISSION_GRANTED) {
+            // TODO: Consider calling
+            //    Activity#requestPermissions
+            // here to request the missing permissions, and then overriding
+            //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+            //                                          int[] grantResults)
+            // to handle the case where the user grants the permission. See the documentation
+            // for Activity#requestPermissions for more details.
+//            return;
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.CALL_PHONE},REQUEST_CALL);
+        }
+        else
+        {
+//                String dial = "tel:"+number;
+            String dial = "tel:8891792177";
+            startActivity(new Intent(Intent.ACTION_CALL, Uri.parse(dial)));
+        }
+//        startActivity(intent);
     }
 }
